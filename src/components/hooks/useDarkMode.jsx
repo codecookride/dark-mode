c27ae3f13965d7a257fb170ad57e5b4f5bd626c9
codecookride <57050292+codecookride@users.noreply.github.com>
2020-03-19 21:03:41 -0700

    final

--- src/components/hooks/useDarkMode.jsx
+++ src/components/hooks/useDarkMode.jsx
@@ -1,14 +1,14 @@
-import { useLocalStorage } from "./useLocalStorage";
-
-export function useDarkMode(key, initialValues) {
-  // inputs = state and handleChanges
-  if (window.localStorage && window.localStorage.getItem(key)) {
-    initialValues = JSON.parse(window.localStorage.getItem(key));
-  }
-  const [values] = useLocalStorage(key, initialValues);
+import React, { useEffect } from "react";
+import {useLocalStorage} from "./useLocalStorage";
 
 
-
-
-  return [values];
+function useDarkMode(key) {
+  const [toggleOnorOff, setOnorOff] = useLocalStorage(key, false);
+  useEffect(() => {
+    toggleOnorOff
+      ? document.body.classList.add("dark-mode")
+      : document.body.classList.remove("dark-mode");
+  }, [toggleOnorOff]);
+  return [toggleOnorOff, setOnorOff];
 }
+export default useDarkMode;
--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -2,14 +2,16 @@ import { useState } from "react";
 
 
 
-export const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
-  });
+  export const useLocalStorage = (key, initialValue) => {
+
+    const [storedValue, setStoredValue] = useState(() => {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    });
   const setValue = value => {
-    setStoredValue(value);
-    window.localStorage.setItem(key, JSON.stringify(value));
-  };
-  return [storedValue, setValue];
+  setStoredValue(value);
+  window.localStorage.setItem(key, JSON.stringify(value));
 };
+    return [storedValue, setValue];
+  };
+
